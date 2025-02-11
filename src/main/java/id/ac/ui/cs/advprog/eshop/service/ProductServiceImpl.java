e2bd6fd043c99ac7e8f38c42a70d10f7357efac0
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-02-11 23:24:51 +0700

    Add Validation to Product

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -17,6 +17,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product create(Product product) {
+        validateProduct(product);
         productRepository.create(product);
         return product;
     }
@@ -27,5 +28,23 @@ public class ProductServiceImpl implements ProductService {
         List<Product> allProduct = new ArrayList<>();
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
+    }
+
+    public void validateProduct(Product product) {
+        List<String> errors = new ArrayList<>();
+
+        if (product.getProductName() == null || product.getProductName().trim().isEmpty()) {
+            errors.add("Product name cannot be empty.");
+        }
+        if (product.getProductName().length() < 3 || product.getProductName().length() > 50) {
+            errors.add("Product name must be between 3 and 50 characters.");
+        }
+        if (product.getProductQuantity() < 1) {
+            errors.add("Quantity must be at least 1.");
+        }
+
+        if (!errors.isEmpty()) {
+            throw new IllegalArgumentException(String.join(", ", errors));
+        }
     }
 }

3a201381b8aa3c654088f9789c6b75d110ac8ea6
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-02-19 18:12:21 +0700

    fix: cleanup redundant public modifiers in ProductService

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,10 +4,10 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface  ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String productId);
-    public Product edit(Product product);
-    public void validateProduct(Product product);
-    public void deleteById(String productId);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String productId);
+    Product edit(Product product);
+    void validateProduct(Product product);
+    void deleteById(String productId);
 }

5b8955a900d97b4c46d3bb4ba18b99e1fe947c25
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-02-19 17:43:14 +0700

    Test: Add PMD rule violations

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.List;
 
 @Controller
 public class ProductController {
